62aaaf58bd16c4850601459559d7af0b686635e1
Isa DUVAN <isaduvan@outlook.com>
2021-01-17 03:59:47 +0300

    rabbit mq tutorial

---
+++ RabbitMqTutorial/Send.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+namespace RabbitMqTutorial
+{
+    class Send
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PUBLISHER");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    
+                    
+                    for(int i = 0; i<100; i++)
+                    {
+                        string message = "Hello World " + i;
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+
+                        Console.WriteLine("Sent {0}", message);
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Reciever/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+namespace Reciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("RECEIVER");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var conn = factory.CreateConnection())
+            {
+                using(var channel = conn.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello", durable: false, exclusive: false,autoDelete:false,arguments:null);
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received {0}", message);
+                    };
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+                    Console.WriteLine("Press a key to exit.");
+                    Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
